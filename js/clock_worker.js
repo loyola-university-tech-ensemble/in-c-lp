202f585b6ee34a117246a78e147e795f718f4343
Griffin Moe <me@griffinmoe.com>
2018-09-02 15:53:33 -0500

    Scheduler mostly works

--- js/clock_worker.js
+++ js/clock_worker.js
@@ -1,6 +1,16 @@
+/*
+ * 150ms is a reasonable clock interval because the shortest possible phrase is
+ * an eighth note in length. Even at 180bpm that duration in seconds is:
+ *
+ *  (240 / 8) / 180 = ~166ms
+ *
+ * We are using this clock to determine when to schedule phrases notes, not to
+ * determine the overall tempo, so 150ms is a safe value when factoring in user
+ * reaction time, minimum phrase duration, and worst-case device profiles.
+ */
 module.exports = function (self) {
-  self.addEventListener('message', function (ev){
-    var startNum = parseInt(ev.data);
-    setInterval(() => self.postMessage([ startNum ]), 1000);
+  self.addEventListener('message', function (ev) {
+    const interval = parseInt(ev.data);
+    setInterval(() => self.postMessage('tick'), interval);
   });
 };
--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 // Globals
-const scheduleLookAhead = 100; //ms
-const bpm = 132;
-const secondsPerBeat = 60.0 / bpm; // s / bpm
+const scheduleLookAhead = 0.02; //s
+const bpm = 120;
+const secondsPerQuarterNote = 60.0 / bpm; // s / bpm
+const secondsPerEighthNote = 30.0 / bpm; // s / bpm
 
-const work = require('webworkify');
 const util = require('./util.js');
 
 const phrases = require('../phrases/phrases.json').map(({ duration, notes }) =>
   ({
     duration: util.parseTimeString(duration, bpm),
     notes: notes.map(note => Object.assign(note, {
-      midi: util.midiToFrequency(note['midi']),
+      frequency: util.midiToFrequency(note['midi']),
       time: util.parseTimeString(note['time'], bpm),
       duration: util.parseTimeString(note['duration'], bpm),
     })),
@@ -18,27 +18,63 @@ const phrases = require('../phrases/phrases.json').map(({ duration, notes }) =>
 );
 
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-let nextNoteTime = audioCtx.currentTime;
+let nextOstinatoTime = audioCtx.currentTime;
+let nextPulseTime = audioCtx.currentTime;
 
-const sinOsc = audioCtx.createOscillator();
-sinOsc.type = 'sawtooth';
-sinOsc.frequency.value = 110;
-sinOsc.start();
+const ostinatoOsc = audioCtx.createOscillator();
+ostinatoOsc.type = 'triangle';
+ostinatoOsc.frequency.value = util.midiToFrequency(60); // C4
+ostinatoOsc.start();
 
-const gainNode = audioCtx.createGain();
-gainNode.gain.value = 0.0;
+const ostinatoGain = audioCtx.createGain();
+ostinatoGain.gain.value = 0.0;
 
-sinOsc.connect(gainNode);
-gainNode.connect(audioCtx.destination);
+ostinatoOsc.connect(ostinatoGain);
+ostinatoGain.connect(audioCtx.destination);
 
-const timer = work(require('./clock_worker.js'));
+const work = require('webworkify');
+const timer = work(require('./clock_worker.js'), bpm);
+const { Synth } = require('./synth.js');
+
+const mySynth = new Synth(audioCtx);
+
+const playPhrase = (phraseNumber, startTime) => {
+  phrases[phraseNumber].notes.forEach(note =>
+    mySynth.playNoteAt(startTime + note.time, note)
+  );
+  return startTime + phrases[phraseNumber].duration;
+};
+
+const checkbox = document.querySelector('#clock');
+
+let playButtonPressed = false;
+const playButton = document.querySelector('#play');
+playButton.onmousedown = () => { playButtonPressed = true; };
+playButton.onmouseup = () => { playButtonPressed = false; };
 
 timer.addEventListener('message', function (ev) {
-  while (nextNoteTime < audioCtx.currentTime + scheduleLookAhead) {
-    gainNode.gain.setTargetAtTime(0.5, nextNoteTime, 0.01);
-    gainNode.gain.setTargetAtTime(0.0, nextNoteTime + 0.05, 0.01);
-    nextNoteTime += secondsPerBeat;
+  while (nextOstinatoTime < audioCtx.currentTime + 0.5) {
+    if (checkbox.checked) {
+      ostinatoGain.gain.setValueAtTime(0, nextOstinatoTime);
+      ostinatoGain.gain.linearRampToValueAtTime(0.9, nextOstinatoTime + 0.01);
+      ostinatoGain.gain.linearRampToValueAtTime(0.0, nextOstinatoTime + secondsPerEighthNote);
+    }
+    nextOstinatoTime += secondsPerQuarterNote;
   }
 });
 
-timer.postMessage(1);
+let endPhraseTime = audioCtx.currentTime;
+
+timer.addEventListener('message', function (ev) {
+  while (nextPulseTime < audioCtx.currentTime + scheduleLookAhead) {
+    if (playButtonPressed && audioCtx.currentTime >= endPhraseTime) {
+      console.log(`playing phrase at: ${nextPulseTime}`);
+      endPhraseTime = playPhrase(24, nextPulseTime);
+      console.log(`end of phrase: ${endPhraseTime}`);
+    }
+    nextPulseTime += secondsPerEighthNote;
+    console.log(`nextPulse: ${nextPulseTime}`);
+  }
+});
+
+timer.postMessage(150);
